82cc22ec13b02cbcee95820ff8934fd9136d2b94
DawidJag <dawid.jagoda@wp.pl>
2021-01-18 17:05:12 +0100

    updating opening of PDF report

--- pdf_creator.py
+++ pdf_creator.py
@@ -3,8 +3,8 @@ from datetime import date
 import os
 import os.path
 # from android.storage import primary_external_storage_path
-import webbrowser
-
+# import webbrowser
+from kivy.utils import platform
 
 
 
@@ -89,11 +89,40 @@ def save_report(settlement_name, payments):
 
     pdf.output(file, 'F')
 
-    webbrowser.open(file)
+    # webbrowser.open(file)
+    launch_webbrowser(file)
 
     # opening the pdf file with report
     # subprocess.Popen([file], shell=True)
 
+def launch_webbrowser(url):
+    import webbrowser
+    if platform == 'android':
+        from jnius import autoclass, cast
+        def open_url(url):
+            PythonActivity = autoclass('org.kivy.android.PythonActivity')
+            activity = PythonActivity.mActivity
+            Intent = autoclass('android.content.Intent')
+            Uri = autoclass('android.net.Uri')
+            browserIntent = Intent()
+            browserIntent.setAction(Intent.ACTION_VIEW)
+            browserIntent.setData(Uri.parse(url))
+            currentActivity = cast('android.app.Activity', activity)
+            currentActivity.startActivity(browserIntent)
 
+        # Web browser support.
+        class AndroidBrowser(object):
+            def open(self, url, new=0, autoraise=True):
+                open_url(url)
+            def open_new(self, url):
+                open_url(url)
+            def open_new_tab(self, url):
+                open_url(url)
+
+        webbrowser.register('android', AndroidBrowser, None, -1)
+
+    webbrowser.open(url)
+
+# launch_webbrowser('http://www.kivy.org')
 
 
